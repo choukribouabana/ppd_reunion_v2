481dbba0c371edd6d5e8f5f7f32c473f335a682b
kaciyounes <fm_kaci@esi.dz>
2021-06-03 10:46:27 +0200

    Add files via upload
    Adding AjoutSalleAdmin

---
+++ frontend/src/components/ajoutsalle/AjoutSalleAdmin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminSearchAppBar from "../page_accueil/AdminSearchAppBar";
+import AjoutSalle from "./AjoutSalle";
+
+export default function () {
+    return(
+        <div>
+            <AdminSearchAppBar />
+            <AjoutSalle />
+        </div>
+    )
+}
