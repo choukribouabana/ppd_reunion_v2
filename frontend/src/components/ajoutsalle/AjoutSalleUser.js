809c3a05af04629cdbbc5b734ec659759d6c37d4
kaciyounes <fm_kaci@esi.dz>
2021-06-03 10:47:03 +0200

    Add files via upload
    Adding AjoutSalleUser

---
+++ frontend/src/components/ajoutsalle/AjoutSalleUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserSearchAppBar from "../page_accueil/UserSearchAppBar";
+import AjoutSalle from "./AjoutSalle";
+
+export default function () {
+    return(
+        <div>
+            <UserSearchAppBar />
+            <AjoutSalle />
+        </div>
+    )
+}
